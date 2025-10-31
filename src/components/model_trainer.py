2e2930ee7ff98b7b9337e1b56ca1a1c97fa04c80
Pronajit Sarkar <spronajit@gmail.com>
2025-10-31 14:16:14 +0530

    model trained

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,22 +42,65 @@ class ModelTrainer:
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
-                "K-Neighbors Classifier": KNeighborsRegressor(),
-                "XGB Classifier": XGBRegressor(),
-                "CatBoost Classifier": CatBoostRegressor(verbose=False),
-                "AdaBoost Classifier": AdaBoostRegressor()
+                "K-Neighbors Regressor": KNeighborsRegressor(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoost Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+                "Linear Regression": LinearRegression()
+            }
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{
+                    'n_neighbors': [5, 7, 9, 11],
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoost Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
             }
 
-            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
+                                                 models=models, params=params)
             
+           
             # To get the best model score from dict
             best_model_score = max(sorted(model_report.values()))   
 
-            # To get the best model name from dict
             best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-            ]   
+               list(model_report.values()).index(best_model_score)
+            ]
             best_model = models[best_model_name]
+
 
             if best_model_score < 0.6:
                 raise CustomException("No best model found")
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -17,24 +18,32 @@ def save_object(file_path, obj):
     except Exception as e:
         raise e
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         
         for i in range(len(models)):
             model = list(models.values())[i]
+            para= params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
             model_name = list(models.keys())[i]
             
-            # Train the model
+            # # Train the model
+            # model.fit(X_train, y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
             
             # Predicting the test set results
+            # y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
-            
-            # Getting the r2 score for the model
-            r2_square = r2_score(y_test, y_test_pred)
-            
-            report[model_name] = r2_square
+
+            # train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
             
         return report
         
